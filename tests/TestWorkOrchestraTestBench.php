5909cced002ac5dfe8cf9decaffc3ac61ad9cba8
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-03 08:20:30 +0900

    Orchestra\Testbenchを使ってテストが実行できる。

---
+++ tests/TestWorkOrchestraTestBench.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+namespace Tests;
+
+use \Orchestra\Testbench\TestCase;
+
+class TestWorkOrchestraTestBench extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
